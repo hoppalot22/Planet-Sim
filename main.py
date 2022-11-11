95a9d82c070b841b02e3ecea7aac63a4c4eef68d
Hoppalot22 <alex.michael.horner@gmail.com>
2022-11-12 02:49:30 +1100

    Added scrolling so you can keep track of action as well as a compass that always points to the largest mass.

--- main.py
+++ main.py
@@ -33,7 +33,10 @@ class Vector2:
         self.x = float(x)
         self.y = float(y)
 
-        self.components = [x, y]
+        self.components = [self.x, self.y]
+
+    def __repr__(self):
+        return f"[{self.x}, {self.y}]"
 
     def __iter__(self):
         self.counter = 0
@@ -75,10 +78,16 @@ class World:
         worlds.append(self)
         self.ID = str(len(worlds))
         self.frameNum = 0
+        self.largestBody = None
 
+        self.mousePos = Vector2(0,0)
         self.window = tk.Tk()
+        self.window.bind("<Configure>", self.UpdateCanvasSize)
         self.window.title(f'Physics: {self.ID}')
-        self.canvas = tk.Canvas(self.window, width=self.boundaries[0], height=self.boundaries[1])
+        self.canvas = tk.Canvas(self.window, width=self.boundaries[0], height=self.boundaries[1], highlightthickness=0)
+        self.canvas.bind("<Button-1>", self.InitScroll)
+        self.canvas.bind("<B1-Motion>", self.Scroll)
+        self.canvas.bind("<MouseWheel>", self.Zoom)
         self.canvas.pack()
 
     def Tick(self):
@@ -87,6 +96,7 @@ class World:
             body.UpdateAccel()
             body.UpdateVelocity()
             body.UpdatePosition()
+            body.CheckCollision()
             # if self.frameNum % 60 == 0:
             #     print(
             #         f"Body ID: {body.ID}, Body Mass: {body.mass}, Body Accel: {body.acceleration} Body velocity: {body.velocity}, Body Position {body.position}")
@@ -100,12 +110,34 @@ class World:
             self.bodies.append(body)
             body.world = self
             body.ID = str(len(self.bodies))
+            if self.largestBody is not None:
+                if body.mass > self.largestBody.mass:
+                    self.largestBody = body
+            else:
+                self.largestBody = body
             if body.position is None:
                 body.position = [int(self.boundaries[0] / 2), int(self.boundaries[1] / 2)]
 
         else:
             raise Exception("Only MassBodies may be added to Worlds")
 
+    def Zoom(self, event):
+        if(event.delta<0):
+            print("Zooming out")
+        else:
+            print("Zooming in")
+    #     for body in self.bodies:
+    #         body.mass += event.delta/120
+
+    def InitScroll(self, event):
+        self.mousePos = Vector2(event.x, event.y)
+    def Scroll(self, event):
+        for body in self.bodies:
+            body.position += Vector2(event.x-self.mousePos.x, event.y - self.mousePos.y)
+        self.mousePos = Vector2(event.x, event.y)
+
+    def UpdateCanvasSize(self, event):
+        self.canvas.config(width = self.window.winfo_width(), height=self.window.winfo_height())
     def RenderWorld(self):
         self.canvas.delete("all")
         for body in self.bodies:
@@ -115,7 +147,11 @@ class World:
                 self.canvas.create_line(body.position[0], body.position[1],body.position[0]+max(20,body.mass)*body.acceleration[0]/VectorMag(body.acceleration), body.position[1]+max(20,body.mass)*body.acceleration[1]/VectorMag(body.acceleration), fill='blue')
             if not (body.velocity[0] == 0 and body.velocity[1] == 0):
                 self.canvas.create_line(body.position[0], body.position[1],body.position[0]+max(20,body.mass)*body.velocity[0]/VectorMag(body.velocity), body.position[1]+max(20,body.mass)*body.velocity[1]/VectorMag(body.velocity), fill='green')
-
+        if self.largestBody is not None:
+            #print("drawing")
+            arrow_coords = VectorNorm(self.largestBody.position)*20
+            self.canvas.create_line(30,30,30 + arrow_coords.x, 30 + arrow_coords.y)
+            self.canvas.create_oval(26,26,34,34, fill='red')
 
 class MassBody:
     def __init__(self, mass, position=None, size=0, velocity=None, shape="circle"):
@@ -133,28 +169,8 @@ class MassBody:
         self.ID = None
 
     def UpdatePosition(self):
-        proposed_position = self.position + self.velocity
-        collision_point, col_body = self.CheckCollision()
-        if collision_point is not None:
-            var1 = 2*VectorDot(self.velocity, VectorNorm(collision_point))
-            var2 = VectorNorm(collision_point)
+        self.position += self.velocity
 
-            selfVelocityMag = VectorMag(self.velocity)
-            bodyVelocityMag = VectorMag(col_body.velocity)
-
-            M1 = self.mass*selfVelocityMag + bodyVelocityMag*col_body.mass
-            K1 = selfVelocityMag*selfVelocityMag*self.mass + bodyVelocityMag*bodyVelocityMag*col_body.mass
-            a = K1 - col_body.mass-col_body.mass*col_body.mass
-            b = 2*M1*col_body.mass/self.mass
-            c = -1*M1/self.mass
-
-            new_body_velocity = (-1*b + math.sqrt(max(b*b-4*a*c, 0)))/(2*a)
-            new_self_velocity = (M1 - col_body.mass*new_body_velocity)/self.mass
-
-            self.velocity = VectorNorm(self.velocity - var2*var1)*new_self_velocity/2
-            self.position += self.velocity
-        else:
-            self.position = proposed_position
 
     def UpdateVelocity(self):
         self.velocity += self.acceleration
@@ -172,27 +188,51 @@ class MassBody:
 
     def CheckCollision(self):
         for body in self.world.bodies:
-            if body is self:
+            if int(body.ID) >= int(self.ID):
                 continue
             else:
+                #print("calling")
                 if self.shape == "circle" and body.shape == "circle":
                     relPos = body.position - self.position
                     if self.mass + body.mass > VectorMag(relPos):
-                        print(f"Planet {self.ID} has collied with planet {body.ID}")
-                        return VectorNorm(relPos)*self.mass, body
+                        # print(f"Planet {self.ID} has collied with planet {body.ID}")
+
+                        normal = VectorNorm(relPos)
+                        reflectedSelfVector = VectorNorm(self.velocity - normal * 2 * VectorDot(self.velocity, normal))
+                        reflectedBodyVector = VectorNorm(body.velocity + normal * 2 * VectorDot(body.velocity, normal*-1))
+
+                        selfVelocityMag = VectorMag(self.velocity)
+                        bodyVelocityMag = VectorMag(body.velocity)
+
+                        M1 = self.mass * selfVelocityMag + bodyVelocityMag * body.mass
+
+                        #print(M1)
+
+                        new_body_velocity = M1/body.mass/2
+                        new_self_velocity = M1/self.mass/2
+
+                        self.position -= self.velocity
+                        body.position -= body.velocity
+
+                        self.velocity = reflectedSelfVector * new_self_velocity# * 0.95
+                        body.velocity = reflectedBodyVector * new_body_velocity# * 0.95
+
+                        self.position -= normal
+                        body.position += normal
+
         return None, None
 
 
 
 def Main():
     myWorld = World(gravity=5, speed=2)
+    # myWorld.AddBody(MassBody(5, position=[700, 500], velocity=[0, 0]))
     myWorld.AddBody(MassBody(20, position=[700, 500], velocity=[0, 0]))
-    myWorld.AddBody(MassBody(20, position=[900, 500], velocity=[0, 0]))
     for i in range(5):
-        myWorld.AddBody(MassBody(random.randrange(3, 10), position=[200*i, 800], velocity=[-.02*random.randrange(0, 100), 0]))
-    myWorld.AddBody(MassBody(4, position=[900, 600], velocity=[0, 1]))
-    # myWorld.AddBody(MassBody(20, position=[800, 500], velocity=[0, 1]))
-    myWorld.AddBody(MassBody(100, position=[500, 500], velocity=[0, -0.2]))
+        myWorld.AddBody(MassBody(random.randrange(5, 15), position=[200*i, 800], velocity=[0, 0]))
+    myWorld.AddBody(MassBody(20, position=[900, 600], velocity=[0, 1]))
+    myWorld.AddBody(MassBody(20, position=[800, 500], velocity=[0, 1]))
+    myWorld.AddBody(MassBody(100, position=[500, 500], velocity=[0, 0]))
 
     myWorld.Tick()
     myWorld.window.mainloop()
